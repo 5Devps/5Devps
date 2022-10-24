2a3e03bae6423571ceacbbb06f5e7f6e92cc9038
felipeReinaA <felipereina1@hotmail.com>
2022-10-24 14:38:24 -0500

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:appejercicio/vistas/vistaprincipal.dart';
+//import 'package:ejercicioclase/vistas/DetallesPOIUno.dart';
+//import 'package:ejercicioclase/vistas/login.dart';
+//import 'package:ejercicioclase/vistas/ListaPOI.dart';
+//import 'package:ejercicioclase/vistas/vistaprincipal.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Inicio());
+}
+
+class Inicio extends StatelessWidget {
+  const Inicio({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image:AssetImage('imagenes/f3.fw.png')
+            )
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          //body: Login(
+          //body: DetallePOIUno(
+          body:VistaPrincipal(
+
+          ),
+        ),
+      ),
+    );
+
+  }
+}
